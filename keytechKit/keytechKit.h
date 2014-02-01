99a07be765c81112aa9a316420b9d50f1b302bb3
Thorsten Claus <thorstenclaus@web.de>
2014-02-01 09:26:29 +0100

    Initial Commit

---
+++ keytechKit/keytechKit.h
@@ -0,0 +1,13 @@
+//
+//  keytechKit.h
+//  keytechKit
+//
+//  Created by Thorsten Claus on 01.02.14.
+//  Copyright (c) 2014 Claus-Software. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface keytechKit : NSObject
+
+@end
---
+++ keytechKit/keytechKit.m
@@ -0,0 +1,13 @@
+//
+//  keytechKit.m
+//  keytechKit
+//
+//  Created by Thorsten Claus on 01.02.14.
+//  Copyright (c) 2014 Claus-Software. All rights reserved.
+//
+
+#import "keytechKit.h"
+
+@implementation keytechKit
+
+@end
