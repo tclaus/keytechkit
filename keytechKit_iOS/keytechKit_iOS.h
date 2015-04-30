8f39c723dab83c89c15d2b168bb491c93dd6a83a
Thorsten Claus <thorstenclaus@web.de>
2015-04-30 14:49:05 +0200

    local changes

---
+++ keytechKit_iOS/keytechKit_iOS.h
@@ -0,0 +1,19 @@
+//
+//  keytechKit_iOS.h
+//  keytechKit_iOS
+//
+//  Created by Thorsten Claus on 28.04.15.
+//  Copyright (c) 2015 Claus-Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for keytechKit_iOS.
+FOUNDATION_EXPORT double keytechKit_iOSVersionNumber;
+
+//! Project version string for keytechKit_iOS.
+FOUNDATION_EXPORT const unsigned char keytechKit_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <keytechKit_iOS/PublicHeader.h>
+
+
